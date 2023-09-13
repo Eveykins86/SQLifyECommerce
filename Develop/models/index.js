cabaacc00ae16b15f85e97386240efcbff60dd84
Evelyn Jimenez <evey.plt@gmail.com>
2023-09-12 19:36:36 -0500

    Establishing relationships between models using Sequelize

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,24 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'SET NULL',
+});
 // Categories have many Products
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 // Products belongToMany Tags (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignkey: 'tag_id',
+});
 // Tags belongToMany Products (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignkey: 'product_id',
+});
 
 module.exports = {
   Product,
